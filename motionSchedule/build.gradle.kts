828653f896a88e5286625b5450f6f6fb36d61172
Jared Guttromson <jaredguttromson@gmail.com>
2024-08-10 19:51:05 -0600

    new version

--- motionSchedule/build.gradle.kts
+++ motionSchedule/build.gradle.kts
@@ -15,7 +15,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.big-jared",
         artifactId = "motion-calendar",
-        version = "0.0.2"
+        version = "0.0.4"
     )
 
     // Configure POM metadata for the published artifact
